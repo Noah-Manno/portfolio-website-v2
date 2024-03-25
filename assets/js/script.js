03a4735a2f7564d2b7ba5f1919f74f75532584a1
Noah Manno <noahmanno19@gmail.com>
2024-03-25 11:47:26 -0400

    back

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,3 @@ const myMusic = document.getElementById('three')
 const github = document.getElementById('four')
 const linkin = document.getElementById('five')
 const contactMe = document.getElementById('six')
-const mainContainer = document.getElementById('container')
-
-aboutMe.addEventListener('click', function() {
-    mainContainer.classList.add('leave')
-})
