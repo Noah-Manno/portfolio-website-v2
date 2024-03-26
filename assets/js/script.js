23232a06759aa71e3297088d7859b0717a6bc33c
Noah Manno <noahmanno19@gmail.com>
2024-03-25 23:20:57 -0400

    new-style

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,7 @@ const myMusic = document.getElementById('three')
 const github = document.getElementById('four')
 const linkin = document.getElementById('five')
 const contactMe = document.getElementById('six')
+
+const bioText = document.getElementById('bio-text')
+
+bioText.textContent = '<Web Developer/>'
