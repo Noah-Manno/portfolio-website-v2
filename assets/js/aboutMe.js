1ca11988c906ef365a6ed71472b34a9da5b95cf5
Noah Manno <noahmanno19@gmail.com>
2024-03-26 10:10:29 -0400

    finished about-me

--- assets/js/aboutMe.js
+++ assets/js/aboutMe.js
@@ -1,3 +1,16 @@
 const bioText = document.getElementById('bio-text')
 
 bioText.textContent = '<Web Developer/>'
+
+const nameEl = document.getElementById('name-box');
+const mainEl = document.getElementById('main');
+const containerEl = document.getElementById('container')
+nameEl.addEventListener('click', function() {
+    mainEl.classList.add('swap-gradient')
+    containerEl.classList.add('swipe-left')
+
+    setTimeout(function(){
+        window.location.href = 'index.html';
+    }, 2000);
+
+});
