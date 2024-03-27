e1569b8617aaf4c7a6250e9636322944e1b1a77a
Noah Manno <noahmanno19@gmail.com>
2024-03-27 16:40:42 -0400

    added hover code on header

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,16 +27,30 @@ contactMe.addEventListener('click', function() {
     
 });   
 
-const openBodyTag = document.getElementById('body-tag')
-const openMainTag = document.getElementById('main-tag')
-const openhTag = document.getElementById('h1-tag')
-const explanationTag = document.getElementById('explanation')
-const endMainTag = document.getElementById('end-main-tag')
-const endBodyTag = document.getElementById('end-body-tag')
+const htmlTag = document.getElementById('html-tag')
+const headTag = document.getElementById('head-tag')
+const titleTag = document.getElementById('title')
+const headTagClose = document.getElementById('head-tag-close')
+const bodyTag = document.getElementById('body-tag')
+const headerTag = document.getElementById('header-tag')
+const webDevTag = document.getElementById('web-dev-tag')
+const headerTagClose = document.getElementById('header-tag-close')
+const mainTag = document.getElementById('main-tag')
+const pTag = document.getElementById('p-tag')
+const mainTagClose = document.getElementById('main-tag-close')
+const bodyTagClose = document.getElementById('body-tag-close')
+const htmlTagClose =document.getElementById('html-tag-close')
 
-openBodyTag.textContent = '<body>'
-openMainTag.textContent = '<main>'
-openhTag.textContent = '<h1> Noah Manno <h1>'
-explanationTag.textContent = '<p> A passionate web developer who makes pixel perfect web interfaces. <p>'
-endMainTag.textContent = '</main>'
-endBodyTag.textContent = '</body>'
+htmlTag.textContent = '<html>' 
+headTag.textContent = '<head>'
+titleTag.textContent = '<title> Noah Manno Portfolio Website </title>' 
+headTagClose.textContent = '</head>' 
+bodyTag.textContent = '<body>' 
+headerTag.textContent = '<header>' 
+webDevTag.textContent = '<Web Developer>' 
+headerTagClose.textContent = '</header>' 
+mainTag.textContent = '<main>' 
+pTag.textContent = '<p> My name is Noah Manno, and I am a passionate web developer who is dedicated to creating pixel perfect web applications. </p>' 
+mainTagClose.textContent = '</main>'
+bodyTagClose.textContent = '</body>' 
+htmlTagClose.textContent = '</html>' 
