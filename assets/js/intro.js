95a4e834a8e91191789208780f8d1090942ad5fc
Noah Manno <noahmanno19@gmail.com>
2024-03-28 10:15:59 -0400

    added loading page

--- assets/js/intro.js
+++ assets/js/intro.js
@@ -1,9 +1,47 @@
-const container = document.getElementById('container')
+const welcome = document.getElementById('welcome')
+let spinner = document.getElementById('absolute');
+let opacity = 0;
+let isFadingIn = true;
 
-container.addEventListener('click', function() {
-    container.classList.add('zoom-away');
+let fade = setInterval(() => {
+    if (isFadingIn) {
+        if (opacity < 1) {
+        spinner.style.opacity = opacity;
+        opacity += 0.01;
+        } else {
+            isFadingIn = false;
+        }
+    } else {
+        setTimeout(function(){
+            if (opacity > 0) {
+                welcome.classList.add('fade-out')
+                spinner.style.opacity = opacity;
+                opacity -= 0.01;
+            } else {
+                clearInterval(fade);
+            }
+        }, 200);
+    }
+}, 10);
 
-    setTimeout(function(){
-        window.location.href = 'index.html';
-    }, 2000);
-});
+const container = document.getElementById('container');
+const title = document.getElementById('title');
+const subtitle = document.getElementById('subtitle');
+
+if (isFadingIn) {
+setTimeout(function(){
+    title.classList.add('drop-in')
+    subtitle.classList.add('drop-in');
+
+    container.addEventListener('click', function() {
+        container.classList.add('zoom-away');
+        setTimeout(function(){
+            window.location.href = 'index.html';
+        }, 2000);
+    });
+}, 3000);
+}
+
+
+
+
