23a36e84c7a09bc91a7e92d08c51ee349e473d65
Noah Manno <noahmanno19@gmail.com>
2024-03-29 12:55:18 -0400

    changed logo animation

--- assets/js/intro.js
+++ assets/js/intro.js
@@ -1,6 +1,28 @@
 let spinner = document.getElementById('absolute');
 let opacity = 0;
 let isFadingIn = true;
+const first = document.getElementById('first')
+const second = document.getElementById('second')
+const third = document.getElementById('third')
+const forth = document.getElementById('forth')
+const logo = document.getElementById('logo')
+
+setTimeout(function(){
+    first.classList.add('visible')  
+}, 200);
+setTimeout(function(){
+    second.classList.add('visible')   
+}, 700);
+setTimeout(function(){
+    third.classList.add('visible')   
+}, 1200);
+setTimeout(function(){
+    forth.classList.add('visible')   
+}, 1700);
+setTimeout(function(){
+    logo.classList.add('out')   
+}, 2500);
+
 
 let fade = setInterval(() => {
     if (isFadingIn) {
